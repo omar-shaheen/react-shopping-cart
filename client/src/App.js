157939d4f6f7ddf30a7e5a44a5085740620f6f33
Omar Shaheen <16275929+omar-shaheen@users.noreply.github.com>
2022-06-06 01:47:00 +0200

    Filter Part 2 + Ended
    Filter Part 2 + Ended

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,55 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
 // import { words } from './words';
-import data from './data.json';
+import data from "./data.json";
 
-import Products from './components/Products/Products';
-import Filter from './components/Filter/Filter';
-
+import Products from "./components/Products/Products";
+import Filter from "./components/Filter/Filter";
 
 function App() {
   // console.log(data);
 
   const [products, setProducts] = useState(data);
   // console.log(products);
+
+  const [sort, setSort] = useState("");
+  const [size, setSize] = useState("");
+
+  const handleFilterBySize = (e) => {
+    // console.log(e.target.value);
+    setSize(e.target.value);
+
+    if (e.target.value == "ALL") {
+      setProducts(data);
+    } else {
+      let productsClone = [...products];
+      // console.log(productsClone);
+      let newProducts = productsClone.filter(p=> p.sizes.indexOf(e.target.value) != -1);
+      setProducts(newProducts)
+    }
+  };
+
+  const handleFilterByOrder = (e) => {
+    // console.log(e.target.value);
+    let order = e.target.value;
+    setSort(order);
+
+    let productsClone = [...products];
+    let newProducts = productsClone.sort((a, b)=> {
+      if (order == "lowest")  {
+        return a.price - b.price;
+      } else if (order == "highest") {
+        return b.price - a.price;
+      } else {
+        return a.id < b.id ? 1 : -1
+      }
+    });
+    // console.log(newProducts);
+    setProducts(newProducts)
+  };
 
   return (
     <div className="layout">
@@ -23,12 +58,16 @@ function App() {
       <main>
         <div className="wrapper">
           <Products products={products} />
-          <Filter />
+          <Filter
+            sort={sort}
+            size={size}
+            handleFilterByOrder={handleFilterByOrder}
+            handleFilterBySize={handleFilterBySize}
+          />
         </div>
       </main>
 
       <Footer />
-
     </div>
   );
 }
--- client/src/components/Filter/Filter.js
+++ client/src/components/Filter/Filter.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../../css/Filter/Filter.css";
 
-export default function Filter() {
+export default function Filter(props) {
   return (
     <div className="filter-wrapper">
       <h2 className="filter-title"> Filter </h2>
@@ -9,19 +9,28 @@ export default function Filter() {
       <hr />
       <div className="filter-by-size">
         <span>Filter</span>
-        <select className="filter-select">
-          <option value="All">All</option>
+        <select
+          className="filter-select"
+          value={props.size}
+          onChange={props.handleFilterBySize}
+        >
+          <option value="ALL">All</option>
           <option value="XL">XL</option>
           <option value="S">S</option>
+          <option value="L">L</option>
           <option value="M">M</option>
         </select>
       </div>
       <div className="filter-by-size">
         <span>Order</span>
-        <select className="filter-select">
+        <select
+          className="filter-select"
+          value={props.sort}
+          onChange={props.handleFilterByOrder}
+        >
           <option value="latest">Latest</option>
-          <option value="lower">Lower</option>
           <option value="highest">Highest</option>
+          <option value="lowest">Lowest</option>
         </select>
       </div>
     </div>
